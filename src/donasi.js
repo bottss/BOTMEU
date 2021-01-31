9ef8cb3e05253a6e4983509381de956cffde0ccb
bottss <78264599+bottss@users.noreply.github.com>
2021-01-30 21:38:35 -0300

    Add files via upload

---
+++ src/donasi.js
@@ -0,0 +1,11 @@
+const donasi = () => {
+	return `
+
+┏━━━━━━━━━━━━━━━━━━━━
+┃    nao da pra doar
+┣━━━━━━━━━━━━━━━━━━━━
+
+`
+}
+
+exports.donasi = donasi
---
+++ src/help.js
@@ -0,0 +1,199 @@
+const help = (prefix) => {
+	return `
+                 
+Author    : ՏᎡ.MARIANO 𒀱
+
+           |━━━━━━━━━━━━━━━━━━
+           |   ┈┈┈╭━━╮┈┈┈┈┈┈
+           |   ┈┈╭╯┊◣╰━━━━╮┈┈
+           |   ┈┈┃┊┊┊╱▽▽▽┛┈┈
+           |   ┈┈┃┊┊┊| 𝙼𝙰𝚁𝙸𝙰𝙽𝙾 𝙱𝙾𝚃𝚂
+           |   ━━╯┊┊┊╲△△△┓┈┈
+           |      ┊┊┊┊╭━━━━━━╯┈┈                              
+            ━━━━━━━━━━━━━━━━━━
+      
+
+
+      ┌─────────❶.❷────────┐
+             ● ᏴϴͲ●ᎷᎬΝႮ●     
+      └─────────❶.❷────────┘
+         ╔═════════════════╗
+          ᏟϴᎷᎪΝᎠϴՏ ᏞᏆՏͲᎪᎠϴՏ
+   
+ 
+        ᎬХᏟᏞႮՏᏆᏙϴ ᏢᎪᎡᎪ ᎪᎠᎷ'Տ
+              ✌(⊙.⊙)✌
+    ╭━━━━━━━━━━━━━━━━━━━━━━━ 
+    ├> !welcome [0/1]
+    ├> !bv [0/1]
+    ├> !bemvindo [0/1]
+    ├> !boasvindas [0/1]
+    ├────> desativar/ativa bv
+       ࿇ ══━━━━━━━✥◈✥━━━━━━━══ ࿇
+    ├> !add 55xxx
+    ├> !adicionar 55xxx
+    ├────> add menbro
+       ࿇ ══━━━━━━━✥◈✥━━━━━━━══ ࿇
+    ├> !kick @membro
+    ├> !ban @membro
+    ├> !remover @membro
+    ├────> remover membro
+       ࿇ ══━━━━━━━✥◈✥━━━━━━━══ ࿇
+    ├> !promote @membro
+    ├> !promover @membro
+    ├────> promover a ADM
+       ࿇ ══━━━━━━━✥◈✥━━━━━━━══ ࿇
+    ├> !demote @adm
+    ├> !promover @adm
+    ├────> rebaixar a membro
+       ࿇ ══━━━━━━━✥◈✥━━━━━━━══ ࿇
+    ├> !grup abrir/fechar
+    ├> !group abrir/fechar
+    ├> !grupo abrir/fechar
+    ├────> bem específico ;-;
+       ࿇ ══━━━━━━━✥◈✥━━━━━━━══ ࿇
+    ├> !setname seu txt
+    ├> !mudarnome seu txt
+    ├────> troca nome do grupo
+       ࿇ ══━━━━━━━✥◈✥━━━━━━━══ ࿇
+    ├> !setdesc seu txt
+    ├> !mudardesc seu txt
+    ├────> troca descrição      
+       ࿇ ══━━━━━━━✥◈✥━━━━━━━══ ࿇
+    ├> !mudarfoto
+     |────> mudar foto do grupo
+       ࿇ ══━━━━━━━✥◈✥━━━━━━━══ ࿇ 
+ -> FAZER MARCAÇÕES DE TODOS OS MEMBROS
+    ├> !marcar
+     |────> marcação com wa.me....
+       ࿇ ══━━━━━━━✥◈✥━━━━━━━══ ࿇
+    ├> !marcar2
+    ├────> marcação 
+       ࿇ ══━━━━━━━✥◈✥━━━━━━━══ ࿇
+    ├> !marcar3
+    ├────> marcação recomendável
+
+  ࿇━━━━━━━━━━━┑^◆^┍━━━━━━━━━━━━ ࿇
+
+    ╔═════════════════════════════
+    ├> ᎠϴᏔΝᏞϴᎪᎠ/ᏞᏆΝK
+       ࿇ ══━━━━━━━✥◈✥━━━━━━━══ ࿇
+    ├> !tiktokstalk Nick da conta
+    ├────> stalk de tiktok 
+       ࿇ ══━━━━━━━✥◈✥━━━━━━━══ ࿇
+    ├> !fototiktok
+    ├────> foto do tiktok
+       ࿇ ══━━━━━━━✥◈✥━━━━━━━══ ࿇
+    ├> !ytmp4 link
+    ├> !ytvideo link
+    ├> !ytbuscar link
+    ├> !ytbaixa link
+    ├────> baixa vídeo do YouTube
+ ࿇━━━━━━━━━━━┑^◆^┍━━━━━━━━━━━━ ࿇
+║ ╔═════════════════════════════
+    ├>ᎷᏆᎠᏆᎪ/ϴႮͲᎡϴՏ
+   
+    ├> !sticker
+    ├> !stiker
+    ├> !fig
+    ├> !figurinha
+    ├────> fazer fig cm foto/gif e com vídeo no mínimo 10 segundos
+       ࿇ ══━━━━━━━✥◈✥━━━━━━━══ ࿇
+    ├> !tts seu txt
+    ├> !gtts seu txt
+    ├> !audio seu txt
+    ├────> converter txt em áudio
+       ࿇ ══━━━━━━━✥◈✥━━━━━━━══ ࿇
+    ├> !toimg marca fig
+    ├> !converter marca fig
+    ├────> converter fig em foto 
+       ࿇ ══━━━━━━━✥◈✥━━━━━━━══ ࿇
+    ├> !txtdafoto
+    ├> !ocr
+    ├────> pega txt de foto
+       ࿇ ══━━━━━━━✥◈✥━━━━━━━══ ࿇
+    ├> !wait marca foto
+    ├> !pesquisar marca foto
+    ├────> pesquisa por anime de ft
+       ࿇ ══━━━━━━━✥◈✥━━━━━━━══ ࿇
+    ├> !listaadms
+    ├> !listadmins
+    ├> !adms
+    ├────> lista dos adms
+       ࿇ ══━━━━━━━✥◈✥━━━━━━━══ ࿇
+    ├> !cachorro
+    ├> !auau
+    ├> !dog
+    ├────> dog fofo aleatório
+       ࿇ ══━━━━━━━✥◈✥━━━━━━━══ ࿇
+    ├> !logo3d seu txt
+    ├────> logo cm seu texto
+       ࿇ ══━━━━━━━✥◈✥━━━━━━━══ ࿇
+    ├> !logoph seu|txt
+    ├────> logo cm seu texto
+       ࿇ ══━━━━━━━✥◈✥━━━━━━━══ ࿇
+    ├> !info
+    ├────> informações do bot
+       ࿇ ══━━━━━━━✥◈✥━━━━━━━══ ࿇
+    ├> !listabloq
+    ├────> números bloqueados
+       ࿇ ══━━━━━━━✥◈✥━━━━━━━══ ࿇
+    ├> !lofi
+    ├────> foto única de lofi
+
+  ࿇━━━━━━━━━━━┑^◆^┍━━━━━━━━━━━━ ࿇
+
+
+
+    ├>ᏟϴᎷᎪΝᎠϴՏ Ꭰϴ ᏟᎡᏆᎪᎠϴᎡ
+       ࿇ ══━━━━━━━✥◈✥━━━━━━━══ ࿇
+    ├> !hidetag
+    ├────> esconde tag
+       ࿇ ══━━━━━━━✥◈✥━━━━━━━══ ࿇
+    ├> !clearall
+    ├> !limpar
+    ├────> limpa zap do bot
+       ࿇ ══━━━━━━━✥◈✥━━━━━━━══ ࿇
+    ├> !bloquear @membro
+    ├────> autobloq
+       ࿇ ══━━━━━━━✥◈✥━━━━━━━══ ࿇
+    ├> !desbloquear
+    ├────> autodesbloq
+       ࿇ ══━━━━━━━✥◈✥━━━━━━━══ ࿇
+    ├> !kitar
+    ├────> bot sai do grupo
+       ࿇ ══━━━━━━━✥◈✥━━━━━━━══ ࿇
+    ├> !bc
+    ├────> [ele faz uma transmissão e manda pra todos os grupos e ctt]
+       ࿇ ══━━━━━━━✥◈✥━━━━━━━══ ࿇
+    ├> !clonar
+    ├> !clone
+    ├────> clonar tag
+       ࿇ ══━━━━━━━✥◈✥━━━━━━━══ ࿇
+╔═════════════════════════════
+├>  ՏႮᏢϴᎡͲᎬ
+       
+    ├> !bug 
+    ├> !reportar
+    ├> !reportarbug
+    ├────> reportar bug ao criador
+
+       ࿇ ══━━━━━━━✥◈✥━━━━━━━══ ࿇
+    ├> !sigla nova sigla
+    ├> !setprefix
+    ├────> troca sigla de comandos
+  ࿇━━━━━━━━━━━┑^◆^┍━━━━━━━━━━━━ ࿇
+╚═════════════════════════════
+┌─────────❶.❷────────┐
+ 𒀱  𝙼𝙰𝚁𝙸𝙰𝙽𝙾 𝙳𝙾𝙼𝙸𝙽𝙰  𒀱
+└─────────❶.❷────────┘
+
+
+`
+}
+
+exports.help = help
+
+
+
+
